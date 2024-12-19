884b612ef052f3b2ae250c06d7e73e8ef7763bd6
Rodrigo Joel Hepp <rodrigo.hepp@unoesc.edu.br>
2024-12-19 20:13:16 -0300

    -feat Adiciona os DTOs, Enum, e o controller para Medicos

---
+++ api/src/main/java/med/voll/api/DTO/medico/DadosCadastroMedico.java
@@ -0,0 +1,8 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.enums.Especialidade;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+
+}
