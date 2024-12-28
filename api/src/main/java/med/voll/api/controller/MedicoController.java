d4b12529c75481f0b6f09ea3753359f32507c631
Rodrigo Joel Hepp <rodrigo.hepp@unoesc.edu.br>
2024-12-28 15:55:21 -0300

    -feat cria o objeto para listagem de médico e adiciona no servive e controller o método para listar os dados e implementa paginação

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.DTO.medico.DadosCadastroMedico;
+import med.voll.api.DTO.medico.DadosListagemMedico;
 import med.voll.api.model.Medico;
 import med.voll.api.service.MedicoService;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @RestController
@@ -23,5 +30,9 @@ public class MedicoController {
     public void cadastrar (@RequestBody @Valid DadosCadastroMedico json) {
         medicoService.salvarMedico(new Medico(json));
     }
-    
+
+    @GetMapping("/listar")
+    public List<DadosListagemMedico> listaMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return medicoService.listarMedicos(paginacao);
+    }
 }
--- api/src/main/java/med/voll/api/service/MedicoService.java
+++ api/src/main/java/med/voll/api/service/MedicoService.java
@@ -1,9 +1,13 @@
 package med.voll.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
+import med.voll.api.DTO.medico.DadosListagemMedico;
 import med.voll.api.model.Medico;
 import med.voll.api.repository.MedicoRepository;
 
@@ -11,10 +15,14 @@ import med.voll.api.repository.MedicoRepository;
 public class MedicoService {
 
     @Autowired
-    private MedicoRepository MedicoRepository;
+    private MedicoRepository medicoRepository;
 
     @Transactional
     public void salvarMedico(Medico medico) {
-        MedicoRepository.save(medico);
+        medicoRepository.save(medico);
+    }
+
+    public List<DadosListagemMedico> listarMedicos(Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).stream().map(DadosListagemMedico::new).toList();
     }
 }
