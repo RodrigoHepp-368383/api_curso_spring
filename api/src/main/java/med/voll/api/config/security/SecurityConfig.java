c2038a6c1fd70c569b9cd4e49fb54c3a136b43d6
Rodrigo Joel Hepp <rodrigo.hepp@unoesc.edu.br>
2025-01-09 11:16:26 -0300

    -feat Altera a estrutura do projeto para as configurações de segurança ficarem em uma pasta propria

--- api/src/main/java/med/voll/api/config/security/SecurityConfig.java
+++ api/src/main/java/med/voll/api/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package med.voll.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,7 +29,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
         .authorizeHttpRequests(req -> {
             req.requestMatchers("/login").permitAll();
             req.anyRequest().authenticated();
-        })
+        }).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
     .build();
 }
 
