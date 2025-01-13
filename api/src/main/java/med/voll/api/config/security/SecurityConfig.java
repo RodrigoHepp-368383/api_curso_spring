0a70c7178d30604b94bf0636912726720b9dda31
Rodrigo Joel Hepp <rodrigo.hepp@unoesc.edu.br>
2025-01-13 16:40:27 -0300

    -config Altera as configurações padrões de segurança, implementando as regras de autenticação

--- api/src/main/java/med/voll/api/config/security/SecurityConfig.java
+++ api/src/main/java/med/voll/api/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package med.voll.api.config;
+package med.voll.api.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
--- api/src/main/java/med/voll/api/config/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/config/security/SecurityFilter.java
@@ -1,4 +1,4 @@
-package med.voll.api.config;
+package med.voll.api.config.security;
 
 import java.io.IOException;
 
